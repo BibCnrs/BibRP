d212435bdfdba30c56cb8bff5a15ffc6fb32be66
ThieryMichel <thiery@marmelab.com>
2016-01-26 11:26:36 +0100

    remove bibcnrs container

--- tmp/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// script utilisé pour afficher
-// tous les headers
-// de la requête HTTP
-
-header('Content-Type: application/json');
-echo json_encode(getallheaders(), JSON_PRETTY_PRINT);
