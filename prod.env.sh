10d42627d603b367c1a6a4308e4a7ea7da7e1899
ThieryMichel <thiery@marmelab.com>
2016-02-04 16:00:00 +0100

    fix prod.env.sh

--- prod.env.sh
+++ prod.env.sh
@@ -3,6 +3,7 @@
 export APPLI_APACHE_SERVERNAME="https://bib.cnrs.fr"
 export APPLI_APACHE_SERVERADMIN="bibcnrs@inist.fr"
 export APPLI_APACHE_LOGLEVEL="info ssl:warn"
+export DOCKER_HOST_IP=`ip -4 addr show docker0 | grep --perl-regexp --only-matching 'inet \K[\d.]+'`
 
 ping -c 1 -W 1 proxyout.inist.fr >/dev/null
 if [ "$?" = "0" ]; then
